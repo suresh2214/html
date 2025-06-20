f26b62007b5de3e74c5cb2f536287695d163b780
suresh_999 <babusureshoruganti@gmail.com>
2025-06-20 11:30:53 +0530

    Create  Arithmeticoperators.java

---
+++ Arithmeticoperators.java
@@ -0,0 +1,12 @@
+public class Arithmeticoperators
+{
+public static void main (String[]args)
+{
+int a=10,b=9;
+System.out.println("a+b="+(a+b));
+System.out.println("a-b="+(a-b));
+System.out.println("a*b="+(a*b));
+System.out.println("a/b="+(a/b));
+System.out.println("a%b="+(a%b));
+}
+}
