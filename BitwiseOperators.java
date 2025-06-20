d9fa5abb5face250b2e23f457d967e0a3ea0ebe9
suresh_999 <babusureshoruganti@gmail.com>
2025-06-20 11:28:12 +0530

    Create BitwiseOperators.java

---
+++ BitwiseOperators.java
@@ -0,0 +1,14 @@
+public class BitwiseOperators
+  {
+    public static void main(String[] args)
+    {
+        int a = 5; 
+      int b = 3;  
+        System.out.println("a & b: " + (a & b)); 
+        System.out.println("a | b: " + (a | b));
+        System.out.println("a ^ b: " + (a ^ b)); 
+        System.out.println("~a: " + (~a));     
+        System.out.println("a << 1: " + (a << 1)); 
+        System.out.println("a >> 1: " + (a >> 1)); 
+    }
+}
